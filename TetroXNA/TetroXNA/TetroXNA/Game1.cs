c119d60c20ac3ce0802c1388bde846ba41050cd3
WhiteWolfzzxx <Jasmine101>
2015-01-21 08:46:35 -0500

    Test Vjubb

--- TetroXNA/TetroXNA/TetroXNA/Game1.cs
+++ TetroXNA/TetroXNA/TetroXNA/Game1.cs
@@ -14,7 +14,7 @@ namespace TetroXNA
 {
     /// <summary>
     /// Repo name is FPS
-    /// Test vjubb
+    /// Test Whitewolfzzxx
     /// This is the main type for your game
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
